a6ef3882bc4f0ddfcb4eb2645dda43eb2ec4d684
syumeikoukyo <kankankan0818@hotmail.com>
2021-03-17 09:09:45 +0900

    change init

--- secure_ml/attack/__init__.py
+++ secure_ml/attack/__init__.py
@@ -4,3 +4,5 @@ from secure_ml.attack.membership_inference import ShadowModel
 from secure_ml.attack.membership_inference import Membership_Inference
 from secure_ml.attack.model_inversion import Model_inversion
 from secure_ml.attack.poison_attack import Poison_attack_sklearn
+from secure_ml.attack.fsha import FSHA
+from secure_ml.attack.fsha_mnist import Resnet, Decoder, Discriminator, Pilot
