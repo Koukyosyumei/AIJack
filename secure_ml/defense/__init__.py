c088cfca2ddb11e3a2629ce6cbfe0a18dcb35b4c
Koukyosyumei <koukyosyumei@hotmail.com>
2021-11-07 01:06:18 +0900

    update __init__

--- secure_ml/defense/__init__.py
+++ secure_ml/defense/__init__.py
@@ -1,3 +1,3 @@
-from .ckks import CKKSEncoder, CKKSPlaintext
+from .ckks import CKKSEncoder, CKKSEncrypter, CKKSPlaintext
 from .optimizer import DPSGD
 from .purifier import Purifier_Cifar10, PurifierLoss
