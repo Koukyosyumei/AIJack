42dcceaec9ac30e7b2f737986c3e9b8f690941b0
Koukyosyumei <koukyosyumei@hotmail.com>
2021-11-06 19:36:28 +0900

    fix the relative path to import

--- secure_ml/attack/__init__.py
+++ secure_ml/attack/__init__.py
@@ -1,9 +1,7 @@
-from secure_ml.attack.base_attack import BaseAttacker
-from secure_ml.attack.evasion_attack import Evasion_attack_sklearn
-from secure_ml.attack.fsha import FSHA
-from secure_ml.attack.fsha_mnist import Decoder, Discriminator, Pilot, Resnet
-from secure_ml.attack.membership_inference import (AttackerModel,
-                                                   Membership_Inference,
-                                                   ShadowModel)
-from secure_ml.attack.model_inversion import Model_inversion
-from secure_ml.attack.poison_attack import Poison_attack_sklearn
+from .base_attack import BaseAttacker
+from .evasion_attack import Evasion_attack_sklearn
+from .fsha import FSHA
+from .fsha_mnist import Decoder, Discriminator, Pilot, Resnet
+from .membership_inference import AttackerModel, Membership_Inference, ShadowModel
+from .model_inversion import Model_inversion
+from .poison_attack import Poison_attack_sklearn
