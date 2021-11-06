d7c965651e1663c306bba89cf75fac2c1fa489ff
Koukyosyumei <koukyosyumei@hotmail.com>
2021-11-06 20:16:50 +0900

    add functions to generate random polynomials

---
+++ secure_ml/defense/ckks/random.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+
+def gen_binary_poly(size):
+    """Generates a polynomial with coeffecients in [0, 1]
+    Args:
+        size: number of coeffcients, size-1 being the degree of the
+            polynomial.
+    Returns:
+        array of coefficients with the coeff[i] being
+        the coeff of x ^ i.
+    """
+    return np.random.randint(0, 2, size, dtype=np.int64)
+
+
+def gen_uniform_poly(size, modulus):
+    """Generates a polynomial with coeffecients being integers in Z_modulus
+    Args:
+        size: number of coeffcients, size-1 being the degree of the
+            polynomial.
+    Returns:
+        array of coefficients with the coeff[i] being
+        the coeff of x ^ i.
+    """
+    return np.random.randint(0, modulus, size, dtype=np.int64)
+
+
+def gen_normal_poly(size):
+    """Generates a polynomial with coeffecients in a normal distribution
+    of mean 0 and a standard deviation of 2, then discretize it.
+    Args:
+        size: number of coeffcients, size-1 being the degree of the
+            polynomial.
+    Returns:
+        array of coefficients with the coeff[i] being
+        the coeff of x ^ i.
+    """
+    return np.int64(np.random.normal(0, 2, size=size))
