f00b3c9d94f6fb66340f004ce24809441cb41423
Koukyosyumei <koukyosyumei@hotmail.com>
2021-11-15 01:02:58 +0900

    refactoring

--- src/secureml/attack/inversion/gan_attack.py
+++ src/secureml/attack/inversion/gan_attack.py
@@ -26,21 +26,44 @@ class GAN_Attack_Client(Client):
         self.device = device
 
         self.discriminator = copy.deepcopy(model)
+        self.discriminator.to(self.device)
 
-    def update_generator(self, dataloader, log_interval=5):
-        for i, data in enumerate(dataloader, 0):
-            self.generator.zero_grad()
-            real_cpu = data[0].to(self.device)
-            b_size = real_cpu.size(0)
-            label = torch.full(
-                (b_size,), self.target_label, dtype=torch.float, device=self.device
-            )
-            noise = torch.randn(b_size, self.nz, 1, 1, device=self.device)
-            fake = self.generator(noise).view(-1)
-            output = self.discriminator(fake).view(-1)
-            loss_generator = self.generator_criterion(output, label)
-            loss_generator.backward()
-            self.generator_optimizer.step()
+        self.noise = torch.randn(1, self.nz, 1, 1, device=self.device)
+
+    def update_generator(self, dataloader, epoch=1, log_interval=5):
+        for i in range(epoch):
+            running_error = 0
+            data_size = 0
+
+            for _, data in enumerate(dataloader, 0):
+                self.generator.zero_grad()
+                real = data[0].to(self.device)
+                b_size = real.size(0)
+                data_size += b_size
+
+                noise = torch.randn(b_size, self.nz, 1, 1, device=self.device)
+                fake = self.generator(noise)
+                output = self.discriminator(fake)
+
+                label = torch.full(
+                    (b_size,), self.target_label, dtype=torch.int64, device=self.device
+                )
+                loss_generator = self.generator_criterion(output, label)
+                loss_generator.backward()
+
+                self.generator_optimizer.step()
+
+                running_error += loss_generator.item()
 
             if i % log_interval == 0:
-                print(f"{i} - {loss_generator.item()}")
+                print(f"epoch {i}: generator loss is {running_error/data_size}")
+
+    def download(self, model_parameters):
+        self.model.load_state_dict(model_parameters)
+        self.discriminator.load_state_dict(model_parameters)
+
+    def attack(self, n):
+        noise = torch.randn(n, self.nz, 1, 1, device=self.device)
+        with torch.no_grad():
+            fake = self.generator(noise)
+        return fake
