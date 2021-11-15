5abc10d47bbfded672a786558362d8cbc0927c88
Koukyosyumei <koukyosyumei@hotmail.com>
2021-11-15 09:47:07 +0900

    refactoring

--- src/secureml/attack/inversion/gan_attack.py
+++ src/secureml/attack/inversion/gan_attack.py
@@ -17,6 +17,19 @@ class GAN_Attack_Client(Client):
         user_id=0,
         device="cpu",
     ):
+        """Implementation of model inversion attack
+           reference https://arxiv.org/abs/1702.07464
+
+        Args:
+            model (torch.nn.Module):
+            target_label(int): index of target class
+            generator (torch.nn.Module): Generator
+            generator_optimizer (torch.optim.Optimizer): optimizer for the generator
+            generator_criterion (function): loss function for the generator
+            nz (int): dimension of latent space of the generator
+            user_id (int): user id
+            device (string): device type (cpu or cuda)
+        """
         super().__init__(model, user_id=user_id)
         self.target_label = target_label
         self.generator = generator
@@ -30,39 +43,56 @@ class GAN_Attack_Client(Client):
 
         self.noise = torch.randn(1, self.nz, 1, 1, device=self.device)
 
-    def update_generator(self, dataloader, epoch=1, log_interval=5):
+    def update_generator(self, batch_size=10, epoch=1, log_interval=5):
+        """Updata the Generator
+
+        Args:
+            batch_size (int): batch size
+            epoch (int): epoch
+            log_interval (int): interval of logging
+        """
+
         for i in range(epoch):
             running_error = 0
-            data_size = 0
+            self.generator.zero_grad()
 
-            for _, data in enumerate(dataloader, 0):
-                self.generator.zero_grad()
-                real = data[0].to(self.device)
-                b_size = real.size(0)
-                data_size += b_size
+            noise = torch.randn(batch_size, self.nz, 1, 1, device=self.device)
+            fake = self.generator(noise)
+            output = self.discriminator(fake)
 
-                noise = torch.randn(b_size, self.nz, 1, 1, device=self.device)
-                fake = self.generator(noise)
-                output = self.discriminator(fake)
+            label = torch.full(
+                (batch_size,), self.target_label, dtype=torch.int64, device=self.device
+            )
+            loss_generator = self.generator_criterion(output, label)
+            loss_generator.backward()
 
-                label = torch.full(
-                    (b_size,), self.target_label, dtype=torch.int64, device=self.device
-                )
-                loss_generator = self.generator_criterion(output, label)
-                loss_generator.backward()
+            self.generator_optimizer.step()
 
-                self.generator_optimizer.step()
-
-                running_error += loss_generator.item()
+            running_error += loss_generator.item()
 
             if i % log_interval == 0:
-                print(f"updating generator - epoch {i}: generator loss is {running_error/data_size}")
+                print(
+                    f"updating generator - epoch {i}: generator loss is {running_error/batch_size}"
+                )
 
     def download(self, model_parameters):
+        """Download the parameters from the server
+
+        Args:
+            model_parameters: global parameters sent by the server
+        """
         self.model.load_state_dict(model_parameters)
         self.discriminator.load_state_dict(model_parameters)
 
     def attack(self, n):
+        """Generate fake images
+
+        Args:
+            n (int): the number of fake images created by the Generator
+
+        Returns:
+            fake: generated fake images
+        """
         noise = torch.randn(n, self.nz, 1, 1, device=self.device)
         with torch.no_grad():
             fake = self.generator(noise)
--- src/secureml/attack/inversion/mi_face.py
+++ src/secureml/attack/inversion/mi_face.py
@@ -4,42 +4,59 @@ from ..base_attack import BaseAttacker
 
 
 class MI_FACE(BaseAttacker):
-    def __init__(self, target_model, input_shape):
-        """implementation of model inversion attack
+    def __init__(
+        self,
+        target_model,
+        input_shape,
+        auxterm_func=lambda x: 0,
+        process_func=lambda x: x,
+    ):
+        """Implementation of model inversion attack
            reference https://dl.acm.org/doi/pdf/10.1145/2810103.2813677
 
         Args:
             target_model: model of the victim
             input_shape: input shapes of taregt model
+            auxterm_func (function): the default is constant function
+            process_func (function): the default is identity function
 
         Attributes:
             target_model: model of the victim
             input_shape: input shapes of taregt model
+            auxterm_func (function): the default is constant function
+            process_func (function): the default is identity function
         """
         super().__init__(target_model)
         self.input_shape = input_shape
+        self.auxterm_func = auxterm_func
+        self.process_func = process_func
 
-    def attack(self, target_label, lam, num_itr, process_func=lambda x: x):
+    def attack(
+        self,
+        target_label,
+        lam,
+        num_itr,
+    ):
         """Execute the model inversion attack on the target model.
 
         Args:
             target_label (int): taregt label
-            lam (float) : step size
-            num_itr (int) : number of iteration
-            process_func (function) : default is identity function
+            lam (float): step size
+            num_itr (int): number of iteration
 
         Returns:
             x_numpy (np.array) :
-            loss ([float]) :
+            log :
         """
         log = []
         x = torch.zeros(self.input_shape, requires_grad=True)
         for i in range(num_itr):
-            c = process_func(1 - self.target_model(x)[:, [target_label]])
+            c = 1 - self.target_model(x)[:, [target_label]] + self.auxterm_func(x)
             c.backward()
             grad = x.grad
             with torch.no_grad():
                 x -= lam * grad
+                x = self.process_func(x)
             log.append(c.item())
 
         x_numpy = x.to("cpu").detach().numpy().copy()
