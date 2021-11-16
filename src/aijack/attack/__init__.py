55eaeab33daafff7799405696f9bdb28ab6d6101
Koukyosyumei <koukyosyumei@hotmail.com>
2021-11-16 18:45:06 +0900

    rename GAN_Attack_Client to GAN_Attack

--- src/aijack/attack/__init__.py
+++ src/aijack/attack/__init__.py
@@ -1,6 +1,6 @@
-from .base_attack import BaseAttacker
-from .evasion import Evasion_attack_sklearn
-from .experiment import FSHA, Decoder, Discriminator, Pilot, Resnet
-from .inversion import MI_FACE, GAN_Attack_Client
-from .membership import AttackerModel, Membership_Inference, ShadowModel
-from .poison import Poison_attack_sklearn
+from .base_attack import BaseAttacker  # noqa: F401
+from .evasion import Evasion_attack_sklearn  # noqa: F401
+from .experiment import FSHA, Decoder, Discriminator, Pilot, Resnet  # noqa: F401
+from .inversion import MI_FACE, GAN_Attack  # noqa: F401
+from .membership import AttackerModel, Membership_Inference, ShadowModel  # noqa: F401
+from .poison import Poison_attack_sklearn  # noqa: F401
--- src/aijack/attack/inversion/__init__.py
+++ src/aijack/attack/inversion/__init__.py
@@ -1,2 +1,2 @@
-from .gan_attack import GAN_Attack_Client
-from .mi_face import MI_FACE
+from .gan_attack import GAN_Attack  # noqa: F401
+from .mi_face import MI_FACE  # noqa: F401
