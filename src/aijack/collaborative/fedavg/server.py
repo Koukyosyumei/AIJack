5843274755a7a1c8a8f2f512df4dea5896b05297
Koukyosyumei <koukyosyumei@hotmail.com>
2021-11-24 18:03:22 +0900

    add FedAvg client and server

---
+++ src/aijack/collaborative/fedavg/server.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+from ..core import BaseServer
+
+
+class FedAvgServer(BaseServer):
+    def __init__(self, clients, global_model, server_id=0):
+        super().__init__(clients, global_model, server_id=server_id)
+
+    def update(self, weight=None):
+        if weight is None:
+            weight = np.ones(self.num_clients) / self.num_clients
+
+        uploaded_parameters = [c.upload() for c in self.clients]
+        averaged_params = uploaded_parameters[0]
+
+        for k in averaged_params.keys():
+            for i in range(0, len(uploaded_parameters)):
+                local_model_params = uploaded_parameters[i]
+                w = weight[i]
+                if i == 0:
+                    averaged_params[k] = local_model_params[k] * w
+                else:
+                    averaged_params[k] += local_model_params[k] * w
+
+        self.global_model.load_state_dict(averaged_params)
+
+    def distribtue(self):
+        for client in self.clients:
+            client.download(self.global_model.state_dict())
