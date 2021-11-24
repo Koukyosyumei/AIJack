d421ffd0e53a55d21b6d25b5d0093e325e5ca62f
Koukyosyumei <koukyosyumei@hotmail.com>
2021-11-24 17:23:09 +0900

    add `transform_list_to_tensor`

---
+++ src/aijack/collaborative/utils.py
@@ -0,0 +1,10 @@
+import numpy as np
+import torch
+
+
+def transform_list_to_tensor(model_params_list):
+    for k in model_params_list.keys():
+        model_params_list[k] = torch.from_numpy(
+            np.asarray(model_params_list[k])
+        ).float()
+    return model_params_list
