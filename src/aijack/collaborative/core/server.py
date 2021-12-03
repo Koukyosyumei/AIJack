4cac60069b9cdf62ff7aad23e74d66cda563e2d7
Koukyosyumei <koukyosyumei@hotmail.com>
2021-12-03 14:08:28 +0900

    fix typo

--- src/aijack/collaborative/core/server.py
+++ src/aijack/collaborative/core/server.py
@@ -2,11 +2,12 @@ import torch
 
 
 class BaseServer(torch.nn.Module):
-    def __init__(self, clients, server_model, servre_id=0):
+    def __init__(self, clients, server_model, server_id=0):
         super().__init__()
         self.clients = clients
-        self.servre_id = servre_id
+        self.server_id = server_id
         self.server_model = server_model
+        self.num_clients = len(clients)
 
     def forward(self, x):
         return self.server_model(x)
