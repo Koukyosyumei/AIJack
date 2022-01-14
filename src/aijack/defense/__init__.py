f5e99ad8317677c5e7f79abe87e85d3e0d1a4e82
Koukyosyumei <koukyosyumei@hotmail.com>
2022-01-14 22:22:11 +0900

    move to manager

--- src/aijack/defense/__init__.py
+++ src/aijack/defense/__init__.py
@@ -1,4 +1,4 @@
 from .ckks import CKKSEncoder, CKKSEncrypter, CKKSPlaintext  # noqa: F401
-from .dp import GeneralMomentAccountant  # noqa: F401
+from .dp import GeneralMomentAccountant, PrivacyManager  # noqa: F401
 from .optimizer import DPSGD  # noqa: F401
 from .purifier import Purifier_Cifar10, PurifierLoss  # noqa: F401
