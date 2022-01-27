de30c534ec4fe840cef6612d9717f275e001c719
Koukyosyumei <koukyosyumei@hotmail.com>
2022-01-27 22:58:36 +0900

    implement `_upload_gradients`

--- src/aijack/collaborative/fedavg/client.py
+++ src/aijack/collaborative/fedavg/client.py
@@ -5,8 +5,14 @@ class FedAvgClient(BaseClient):
     def __init__(self, model, user_id=0):
         super().__init__(model, user_id=user_id)
 
-    def upload(self):
+    def _upload_parameters(self):
         return self.model.state_dict()
 
-    def download(self, model_parameters):
+    def _upload_gradients(self):
+        gradients = []
+        for param, prev_param in zip(self.model.parameters(), self.prev_parameters):
+            gradients.append((param.reshape(-1) - prev_param.reshape(-1)).tolist())
+        return gradients
+
+    def _download_parameters(self, model_parameters):
         self.model.load_state_dict(model_parameters)
