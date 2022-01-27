97f4af801c705e1d4dcb993f6ed129a0ef424302
Koukyosyumei <koukyosyumei@hotmail.com>
2022-01-27 23:29:30 +0900

    implements update_from_gradients and add initial communication

--- src/aijack/collaborative/fedavg/server.py
+++ src/aijack/collaborative/fedavg/server.py
@@ -1,4 +1,5 @@
 import numpy as np
+import torch
 
 from ..core import BaseServer
 
@@ -6,12 +7,39 @@ from ..core import BaseServer
 class FedAvgServer(BaseServer):
     def __init__(self, clients, global_model, server_id=0):
         super().__init__(clients, global_model, server_id=server_id)
+        self.distribtue()
 
-    def update(self, weight=None):
+    def action(self, gradients=True):
+        if gradients:
+            self.updata_from_gradients()
+        else:
+            self.update_from_parameters()
+        self.distribtue()
+
+    def updata_from_gradients(self, weight=None):
         if weight is None:
             weight = np.ones(self.num_clients) / self.num_clients
 
-        uploaded_parameters = [c.upload() for c in self.clients]
+        uploaded_gradients = [c.upload_gradients() for c in self.clients]
+        aggregated_gradients = [
+            torch.zeros_like(params) for params in self.server_model.parameters()
+        ]
+        len_gradients = len(aggregated_gradients)
+
+        for gradients in uploaded_gradients:
+            for gradient_id in range(len_gradients):
+                aggregated_gradients[gradient_id] += (1 / self.num_clients) * gradients[
+                    gradient_id
+                ]
+
+        for params, grads in zip(self.server_model.parameters(), aggregated_gradients):
+            params.data += grads
+
+    def update_from_parameters(self, weight=None):
+        if weight is None:
+            weight = np.ones(self.num_clients) / self.num_clients
+
+        uploaded_parameters = [c.upload_parameters() for c in self.clients]
         averaged_params = uploaded_parameters[0]
 
         for k in averaged_params.keys():
