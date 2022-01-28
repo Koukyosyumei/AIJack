02dce4e6089266f75962943573a5dce2bf38da72
Koukyosyumei <koukyosyumei@hotmail.com>
2022-01-28 20:57:10 +0900

    implement total_variations

---
+++ src/aijack/utils/metrics.py
@@ -0,0 +1,7 @@
+import torch
+
+
+def total_variation(x):
+    dx = torch.mean(torch.abs(x[:, :, :, :-1] - x[:, :, :, 1:]))
+    dy = torch.mean(torch.abs(x[:, :, :-1, :] - x[:, :, 1:, :]))
+    return dx + dy
