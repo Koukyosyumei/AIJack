7ceb25a1fe372eaaa644860b2a79cb1a492f834e
Koukyosyumei <koukyosyumei@hotmail.com>
2022-01-29 23:11:06 +0900

    implement soteria

---
+++ src/aijack/defense/setoria/setoria_client.py
@@ -0,0 +1,78 @@
+import torch
+
+from ...collaborative import FedAvgClient
+
+
+class SetoriaFedAvgClient(FedAvgClient):
+    def __init__(
+        self, model, input_layer, perturbed_layer, user_id=0, lr=0.1, epsilon=0.2
+    ):
+        super().__init__(model, user_id=user_id, lr=lr)
+        self.input_layer = input_layer
+        self.perturbed_layer = perturbed_layer
+        self.epsilon = epsilon
+
+        self._set_hook()
+
+    def _set_hook(self):
+        self.inputs = {}
+        self.outputs = {}
+
+        def get_input(name):
+            def hook(net, x, output):
+                if not x.requires_grad:
+                    raise ValueError("x.requires_grad should be True")
+                self.inputs[name] = x
+
+            return hook
+
+        def get_output(name):
+            def hook(net, x, output):
+                self.outputs[name] = output
+
+            return hook
+
+        getattr(self.net, self.input_layer).register_forward_hook(
+            get_input(self.input_layer)
+        )
+        getattr(self.net, self.perturbed_layer).register_forward_hook(
+            get_output(self.perturbed_layer)
+        )
+
+    def action_before_lossbackward(self):
+        input_data = self.inputs[self.input_layer][0]
+        feature = self.outputs[self.perturbed_layer]
+
+        mask = torch.zeros_like(feature)
+        r_dfr_dx_norm = torch.zeros_like(feature)
+
+        rep_size = feature.shape[1]
+        for i in range(rep_size):
+            mask[:, i] = 1
+            feature.backward(
+                mask, retain_graph=True
+            )  # culc the derivative of feature_2 @ df_dtarget
+            dfri_dx = input_data.grad.data
+            r_dfr_dx_norm[:, i] = feature[:, i] / torch.norm(
+                dfri_dx.view(dfri_dx.shape[0], -1), dim=1
+            )
+
+            self.net.zero_grad()
+            input_data.grad.data.zero_()
+            mask[:, i] = 0
+
+        self.topk_idxs = torch.topk(
+            r_dfr_dx_norm.mean(dim=0), int(rep_size * self.epsilon)
+        )[1]
+
+    def action_after_lossbackward(self, target_layer_name=None):
+        target_layer_name = (
+            f"{self.perturbed_layer}.weight"
+            if target_layer_name is None
+            else target_layer_name
+        )
+        dl_dw = {
+            layer_name: params.grad
+            for layer_name, params in zip(self.net.state_dict(), self.net.parameters())
+        }
+        dl_dw[target_layer_name][self.topk_idxs, :] = 0
