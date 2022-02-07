59de695265781b231817ffb3554529a206cbb573
Koukyosyumei <koukyosyumei@hotmail.com>
2022-02-07 10:13:36 +0900

    add mid

--- src/aijack/defense/__init__.py
+++ src/aijack/defense/__init__.py
@@ -1,5 +1,5 @@
 from .ckks import CKKSEncoder, CKKSEncrypter, CKKSPlaintext  # noqa: F401
 from .dp import GeneralMomentAccountant, PrivacyManager  # noqa: F401
-from .mid import VIB  # noqa:F401
+from .mid import VIB, KL_between_normals, mib_loss  # noqa:F401
 from .purifier import Purifier_Cifar10, PurifierLoss  # noqa: F401
 from .setoria import SetoriaFedAvgClient  # noqa: F401
