65623b150f3bee24e944f9f62f65ed3285ec6b7d
Koukyosyumei <koukyosyumei@hotmail.com>
2022-02-10 17:06:24 +0900

    implement `update` method

--- src/aijack/collaborative/fedavg/client.py
+++ src/aijack/collaborative/fedavg/client.py
@@ -4,13 +4,20 @@ from ..core import BaseClient
 
 
 class FedAvgClient(BaseClient):
-    def __init__(self, model, user_id=0, lr=0.1):
+    def __init__(self, model, user_id=0, lr=0.1, send_gradient=True):
         super(FedAvgClient, self).__init__(model, user_id=user_id)
         self.lr = lr
+        self.send_gradient = send_gradient
 
         self.prev_parameters = []
         for param in self.model.parameters():
             self.prev_parameters.append(copy.deepcopy(param))
+
+    def upload(self):
+        if self.send_gradient:
+            return self.upload_gradients()
+        else:
+            return self.upload_parameters()
 
     def upload_parameters(self):
         return self.model.state_dict()
