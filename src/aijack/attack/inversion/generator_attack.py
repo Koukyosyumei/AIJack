ba84a58d58e18954f4e122bbfe1a7db919eb640f
Koukyosyumei <koukyosyumei@hotmail.com>
2022-02-11 10:30:17 +0900

    improve logging and add early_stopping

--- src/aijack/attack/inversion/generator_attack.py
+++ src/aijack/attack/inversion/generator_attack.py
@@ -3,15 +3,24 @@ from ..base_attack import BaseAttacker
 
 class Generator_Attack(BaseAttacker):
     def __init__(
-        self, target_model, attacker_model, attacker_optimizer, log_interval=1
+        self,
+        target_model,
+        attacker_model,
+        attacker_optimizer,
+        log_interval=1,
+        early_stopping=5,
     ):
         super().__init__(target_model=target_model)
         self.attacker_model = attacker_model
         self.attacker_optimizer = attacker_optimizer
         self.log_interval = log_interval
+        self.early_stopping = early_stopping
 
     def fit(self, dataloader, epoch, x_pos=0):
+        best_loss = float("inf")
+        best_epoch = 0
         for i in range(epoch):
+            running_loss = 0
             for data in dataloader:
                 x = data[x_pos]
                 self.attacker_optimizer.zero_grad()
@@ -20,9 +29,17 @@ class Generator_Attack(BaseAttacker):
                 loss = ((x - attack_outputs) ** 2).mean()
                 loss.backward()
                 self.attacker_optimizer.step()
+                running_loss += loss.item() / len(dataloader)
 
             if self.log_interval != 0 and i % self.log_interval == 0:
-                print(f"epoch {i}: reconstruction_loss {loss.item()}")
+                print(f"epoch {i}: reconstruction_loss {running_loss}")
+
+            if running_loss < best_loss:
+                best_loss = running_loss
+                best_epoch = i
+            else:
+                if i - best_epoch > self.early_stopping:
+                    break
 
     def attack(self, data_tensor):
         return self.attacker_model(data_tensor)
