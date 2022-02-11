3a4a2dd994087ad0d4638b688e8123685ea16fe8
Koukyosyumei <koukyosyumei@hotmail.com>
2022-02-11 18:28:32 +0900

    allow multi target models

--- src/aijack/attack/inversion/generator_attack.py
+++ src/aijack/attack/inversion/generator_attack.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from ..base_attack import BaseAttacker
 
 
@@ -11,12 +13,19 @@ class Generator_Attack(BaseAttacker):
         early_stopping=5,
         device="cpu",
     ):
-        super().__init__(target_model=target_model)
+        if type(target_model) == List:
+            super().__init__(target_model=target_model[0])
+        else:
+            super().__init__(target_model=target_model)
         self.attacker_model = attacker_model
         self.attacker_optimizer = attacker_optimizer
         self.log_interval = log_interval
         self.early_stopping = early_stopping
         self.device = device
+
+        self.target_model_list = (
+            target_model if type(target_model) == list else [target_model]
+        )
 
     def fit(self, dataloader, epoch, x_pos=0, y_pos=1, arbitrary_y=False):
         best_loss = float("inf")
@@ -26,15 +35,17 @@ class Generator_Attack(BaseAttacker):
             for data in dataloader:
                 x = data[x_pos]
                 x = x.to(self.device)
-                target_outputs = data[y_pos] if arbitrary_y else self.target_model(x)
-                target_outputs = target_outputs.to(self.device)
 
-                self.attacker_optimizer.zero_grad()
-                attack_outputs = self.attacker_model(target_outputs)
-                loss = ((x - attack_outputs) ** 2).mean()
-                loss.backward()
-                self.attacker_optimizer.step()
-                running_loss += loss.item() / len(dataloader)
+                for target_model in self.target_model_list:
+                    target_outputs = data[y_pos] if arbitrary_y else target_model(x)
+                    target_outputs = target_outputs.to(self.device)
+
+                    self.attacker_optimizer.zero_grad()
+                    attack_outputs = self.attacker_model(target_outputs)
+                    loss = ((x - attack_outputs) ** 2).mean()
+                    loss.backward()
+                    self.attacker_optimizer.step()
+                    running_loss += loss.item() / len(dataloader)
 
             if self.log_interval != 0 and i % self.log_interval == 0:
                 print(f"epoch {i}: reconstruction_loss {running_loss}")
