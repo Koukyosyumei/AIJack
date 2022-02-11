86da9246e10b3a9957713e55762e8b2fbbc9f64a
Koukyosyumei <koukyosyumei@hotmail.com>
2022-02-11 19:02:11 +0900

    fix the position of optimizer.step

--- src/aijack/attack/inversion/generator_attack.py
+++ src/aijack/attack/inversion/generator_attack.py
@@ -27,20 +27,6 @@ class Generator_Attack(BaseAttacker):
             target_model if type(target_model) == list else [target_model]
         )
 
-    def culc_loss(self, dataloader, x_pos=0, y_pos=0, arbitrary_y=False):
-        for data in dataloader:
-            x = data[x_pos]
-            x = x.to(self.device)
-
-            loss = 0
-            for target_model in self.target_model_list:
-                target_outputs = data[y_pos] if arbitrary_y else target_model(x)
-                target_outputs = target_outputs.to(self.device)
-                attack_outputs = self.attacker_model(target_outputs)
-                loss = loss + ((x - attack_outputs) ** 2).mean()
-
-            return loss / len(dataloader)
-
     def fit(self, dataloader, epoch, x_pos=0, y_pos=1, arbitrary_y=False):
         best_loss = float("inf")
         best_epoch = 0
@@ -51,20 +37,20 @@ class Generator_Attack(BaseAttacker):
                 x = x.to(self.device)
 
                 loss = 0
+                self.attacker_optimizer.zero_grad()
                 for target_model in self.target_model_list:
                     target_outputs = data[y_pos] if arbitrary_y else target_model(x)
                     target_outputs = target_outputs.to(self.device)
 
-                    self.attacker_optimizer.zero_grad()
                     attack_outputs = self.attacker_model(target_outputs)
                     loss = loss + ((x - attack_outputs) ** 2).mean()
 
-                loss.backward()
+                loss.backward(retain_graph=True)
                 self.attacker_optimizer.step()
-                running_loss = running_loss + loss.item() / len(dataloader)
+                running_loss = running_loss + loss / len(dataloader)
 
             if self.log_interval != 0 and i % self.log_interval == 0:
-                print(f"epoch {i}: reconstruction_loss {running_loss}")
+                print(f"epoch {i}: reconstruction_loss {running_loss.item()}")
 
             if running_loss < best_loss:
                 best_loss = running_loss
