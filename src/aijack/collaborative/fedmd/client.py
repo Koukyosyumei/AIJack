75ba995e969a32afc40d96b352c225e4e63c70f4
Koukyosyumei <koukyosyumei@hotmail.com>
2022-03-01 11:25:57 +0900

    re-parametize consensus_optimizer

--- src/aijack/collaborative/fedmd/client.py
+++ src/aijack/collaborative/fedmd/client.py
@@ -9,7 +9,6 @@ class FedMDClient(BaseClient):
         self,
         model,
         public_data,
-        consensus_optimizer,
         transform=lambda x: x,
         batch_size=8,
         user_id=0,
@@ -18,7 +17,6 @@ class FedMDClient(BaseClient):
     ):
         super(FedMDClient, self).__init__(model, user_id=user_id)
         self.public_data = public_data
-        self.consensus_optimizer = consensus_optimizer
         self.transform = transform
         self.batch_size = batch_size
         self.base_loss_func = base_loss_func
@@ -36,7 +34,7 @@ class FedMDClient(BaseClient):
     def download(self, predicted_values_of_server):
         self.predicted_values_of_server = predicted_values_of_server
 
-    def approach_consensus(self):
+    def approach_consensus(self, consensus_optimizer):
         running_loss = 0
         for x, y_consensus in torch.utils.data.DataLoader(
             torch.utils.data.TensorDataset(
@@ -44,11 +42,11 @@ class FedMDClient(BaseClient):
             )
         ):
             x = self.transform(x)
-            self.consensus_optimizer.zero_grad()
+            consensus_optimizer.zero_grad()
             y_pred = self(x)
             loss = self.consensus_loss_func(y_pred, y_consensus)
             loss.backward()
-            self.consensus_optimizer.step()
+            consensus_optimizer.step()
             running_loss += loss.item()
 
         return running_loss
