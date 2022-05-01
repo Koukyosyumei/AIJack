d3d6c7be0b627bccab3bae9fd6eddfb9108b8801
Koukyosyumei <koukyosyumei@hotmail.com>
2022-05-01 17:18:32 +0900

    fix typos

--- src/aijack/attack/evasion/__init__.py
+++ src/aijack/attack/evasion/__init__.py
@@ -1,4 +1,4 @@
-"""Submodule for evasion attack, which creates a malicious data that the target
+"""Subpackage for evasion attack, which creates a malicious data that the target
 machine learning model cannot correctly classify.
 """
 from .evasion_attack import Evasion_attack_sklearn  # noqa: F401
