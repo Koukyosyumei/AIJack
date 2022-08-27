773ef8e6aaecca22c48555c02b474f92b4905734
Koukyosyumei <koukyosyumei@hotmail.com>
2022-08-27 14:27:19 +0900

    update

--- src/aijack/collaborative/tree/main.cpp
+++ src/aijack/collaborative/tree/main.cpp
@@ -7,6 +7,8 @@
 
 #include "xgboost/xgboost.h"
 #include "secureboost/secureboost.h"
+#include "../../defense/paillier/src/paillier.h"
+#include "../../defense/paillier/src/keygenerator.h"
 
 #define STRINGIFY(x) #x
 #define MACRO_STRINGIFY(x) STRINGIFY(x)
@@ -22,6 +24,37 @@ PYBIND11_MODULE(aijack_secureboost, m)
     m.doc() = R"pbdoc(
         core of XGBoost
     )pbdoc";
+
+    py::class_<PaillierKeyGenerator>(m, "PaillierKeyGenerator")
+        .def(py::init<int>())
+        .def("generate_keypair", &PaillierKeyGenerator::generate_keypair);
+
+    py::class_<PaillierPublicKey>(m, "PaillierPublicKey")
+        .def("encrypt", &PaillierPublicKey::encrypt<int>)
+        .def("encrypt", &PaillierPublicKey::encrypt<long>)
+        .def("encrypt", &PaillierPublicKey::encrypt<float>)
+        .def("encrypt", &PaillierPublicKey::encrypt<double>)
+        .def("get_publickeyvalues", &PaillierPublicKey::get_publickeyvalues);
+
+    py::class_<PaillierCipherText>(m, "PaillierCipherText")
+        .def("__add__", overload_cast_<int>()(&PaillierCipherText::operator+))
+        .def("__add__", overload_cast_<long>()(&PaillierCipherText::operator+))
+        .def("__add__", overload_cast_<float>()(&PaillierCipherText::operator+))
+        .def("__add__", overload_cast_<double>()(&PaillierCipherText::operator+))
+        .def("__add__", overload_cast_<PaillierCipherText>()(&PaillierCipherText::operator+))
+        .def("__mul__", overload_cast_<int>()(&PaillierCipherText::operator*))
+        .def("__mul__", overload_cast_<long>()(&PaillierCipherText::operator*))
+        .def("__mul__", overload_cast_<float>()(&PaillierCipherText::operator*))
+        .def("__mul__", overload_cast_<double>()(&PaillierCipherText::operator*))
+        .def("get_value", &PaillierCipherText::get_value);
+
+    py::class_<PaillierSecretKey>(m, "PaillierSecretKey")
+        .def("decrypt2int", &PaillierSecretKey::decrypt2int)
+        .def("decrypt2long", &PaillierSecretKey::decrypt2long)
+        .def("decrypt2float", &PaillierSecretKey::decrypt2float)
+        .def("decrypt2double", &PaillierSecretKey::decrypt2double)
+        .def("get_publickeyvalues", &PaillierSecretKey::get_publickeyvalues)
+        .def("get_secretkeyvalues", &PaillierSecretKey::get_secretkeyvalues);
 
     py::class_<XGBoostParty>(m, "XGBoostParty")
         .def(py::init<vector<vector<float>>, int, vector<int>, int,
