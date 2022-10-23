f74b57283de718b37b99cfc6468fb0e0ea6af44f
Koukyosyumei <koukyosyumei@hotmail.com>
2022-10-23 09:17:52 +0900

    update

--- src/aijack/collaborative/tree/main.cpp
+++ src/aijack/collaborative/tree/main.cpp
@@ -1,6 +1,8 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 #include <pybind11/functional.h>
+#include <mpi.h>
+#include <mpi4py/mpi4py.h>
 #include <cmath>
 #include <iostream>
 #include <vector>
@@ -19,6 +21,56 @@ namespace py = pybind11;
 
 template <typename... Args>
 using overload_cast_ = pybind11::detail::overload_cast_impl<Args...>;
+
+struct mpi4py_comm
+{
+    mpi4py_comm() = default;
+    mpi4py_comm(MPI_Comm value) : value(value) {}
+    operator MPI_Comm() { return value; }
+
+    MPI_Comm value;
+};
+
+namespace pybind11
+{
+    namespace detail
+    {
+        template <>
+        struct type_caster<mpi4py_comm>
+        {
+        public:
+            PYBIND11_TYPE_CASTER(mpi4py_comm, _("mpi4py_comm"));
+
+            // Python -> C++
+            bool load(handle src, bool)
+            {
+                PyObject *py_src = src.ptr();
+
+                // Check that we have been passed an mpi4py communicator
+                if (PyObject_TypeCheck(py_src, &PyMPIComm_Type))
+                {
+                    // Convert to regular MPI communicator
+                    value.value = *PyMPIComm_Get(py_src);
+                }
+                else
+                {
+                    return false;
+                }
+
+                return !PyErr_Occurred();
+            }
+
+            // C++ -> Python
+            static handle cast(mpi4py_comm src,
+                               return_value_policy /* policy */,
+                               handle /* parent */)
+            {
+                // Create an mpi4py handle
+                return PyMPIComm_New(src.value);
+            }
+        };
+    }
+}
 
 PYBIND11_MODULE(aijack_secureboost, m)
 {
