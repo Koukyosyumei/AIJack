0160eaf7328940e1fb96d82cf8d8c7d5cce85b8a
Koukyosyumei <koukyosyumei@hotmail.com>
2022-10-24 09:12:11 +0900

    Update

--- generate_docs.sh
+++ generate_docs.sh
@@ -1,3 +1,2 @@
-pandoc -f markdown -t rst -o docs_src/README.rst README.md
-sphinx-apidoc -M -f -o ./docs_src ./src/aijack
-sphinx-build -b html docs_src docs
+jupyter-book build docs_src --path-output docs
+ghp-import -n -p -f docs/_build/html
