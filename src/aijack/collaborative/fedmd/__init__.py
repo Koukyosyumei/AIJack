eac5d9f275d10b19862933463369414b43481afe
Koukyosyumei <koukyosyumei@hotmail.com>
2023-01-01 20:07:56 +0900

    update

--- src/aijack/collaborative/fedmd/__init__.py
+++ src/aijack/collaborative/fedmd/__init__.py
@@ -1,8 +1,8 @@
 from .api import FedMDAPI, MPIFedMDAPI  # noqa : F401
-from .client import FedMDClient, MPIFedMDClient  # noqa : F401
+from .client import FedMDClient, MPIFedMDClientManager  # noqa : F401
 from .nfdp import (  # noqa : F401
     get_delta_of_fedmd_nfdp,
     get_epsilon_of_fedmd_nfdp,
     get_k_of_fedmd_nfdp,
 )
-from .server import FedMDServer, MPIFedMDServer  # noqa : F401
+from .server import FedMDServer, MPIFedMDServerManager  # noqa : F401
