18a1c91eaf810a5054cd6ea800064110cf47b565
Koukyosyumei <koukyosyumei@hotmail.com>
2023-01-03 01:53:12 +0900

    0.0.1-alpha.1

--- setup.py
+++ setup.py
@@ -126,13 +126,13 @@ def read_requirements():
     return requirements
 
 
-__version__ = "0.0.1"
+__version__ = "0.0.1-alpha.1"
 ext_modules = [CMakeExtension("aijack_cpp_core")]
 console_scripts = []
 
 setup(
     name="aijack",
-    version="0.0.1",
+    version="0.0.1-alpha.1",
     ext_modules=ext_modules,
     entry_points={"console_scripts": console_scripts},
     cmdclass={"build_ext": CMakeBuild},
