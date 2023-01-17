8e65afcefacac8fb5098701c41d4cb115dab90b0
Koukyosyumei <koukyosyumei@hotmail.com>
2023-01-17 16:58:20 +0900

    update

--- src/aijack/defense/kanonymity/wrapper.py
+++ src/aijack/defense/kanonymity/wrapper.py
@@ -30,7 +30,9 @@ def convert_anodataframe_to_pddataframe(ano_df, is_continuous_map):
 class Mondrian:
     """Implementation of K. LeFevre, D. J. DeWitt and R. Ramakrishnan,
     'Mondrian Multidimensional K-Anonymity,' 22nd International Conference on Data Engineering
-    (ICDE'06), Atlanta, GA, USA, 2006, pp. 25-25, doi: 10.1109/ICDE.2006.101."""
+    (ICDE'06), Atlanta, GA, USA, 2006, pp. 25-25, doi: 10.1109/ICDE.2006.101. Our implementation
+    is based on Nuclearstar/K-Anonymity (https://github.com/Nuclearstar/K-Anonymity)
+    """
 
     def __init__(self, k=3):
         self.api = MondrianCore(k)
@@ -39,6 +41,8 @@ class Mondrian:
         return self.api.get_final_partitions()
 
     def anonymize(self, df, feature_columns, sensitive_column, is_continuous_map):
+        df_used = df[feature_columns + [sensitive_column]]
+        df_unused = df[list(set(df.columns) - set(df_used.columns))]
         ano_df = convert_pddataframe_to_anodataframe(df, is_continuous_map)
         ano_anonymized_df = self.api.anonymize(
             ano_df, feature_columns, sensitive_column
@@ -46,4 +50,5 @@ class Mondrian:
         pd_anonymized_df = convert_anodataframe_to_pddataframe(
             ano_anonymized_df, is_continuous_map
         )
-        return pd_anonymized_df
+        result_df = pd.concat([pd_anonymized_df, df_unused])
+        return result_df
