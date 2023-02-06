c6d68c74cac770a494a974e69a25535d2c24df3d
Koukyosyumei <koukyosyumei@hotmail.com>
2023-02-06 17:46:45 +0900

    Implement AdaDPS

---
+++ src/aijack/defense/dp/manager/adadps.py
@@ -0,0 +1,128 @@
+import torch
+from .dpoptimizer import (
+    _privatize_lot_grads,
+    _calculate_clip_coef,
+    _apply_clip_coef,
+    _clear_accumulated_grads,
+)
+
+
+def _update_side_info_rmsprop(opt):
+    for group in opt.param_groups:
+        for param, si in zip(group["params"], group["side_information"]):
+            if param.requires_grad:
+                si = (opt.beta) * si + (1 - opt.beta) * param.grad.data**2
+
+
+def _apply_side_infor_rmsprop(opt):
+    for group in opt.param_groups:
+        for param, si in zip(group["params"], group["side_information"]):
+            if param.requires_grad:
+                param.grad.data.div_(torch.sqrt(si))
+
+
+def _update_side_info_adam(opt):
+    for group in opt.param_groups:
+        for param, si, pm in zip(
+            group["params"], group["side_information"], group["potential_momentum"]
+        ):
+            if param.requires_grad:
+                si = (opt.beta) * si + (1 - opt.beta) * param.grad.data**2
+                pm = (opt.beta) * pm + (1 - opt.beta) * param.grad.data
+
+
+def _apply_side_infor_adam(opt):
+    for group in opt.param_groups:
+        for param, si, pm in zip(
+            group["params"], group["side_information"], group["potential_momentum"]
+        ):
+            if param.requires_grad:
+                param.grad.data.mul_(pm / torch.sqrt(si))
+
+
+def _precondition_grads_with_side_info(opt):
+    if opt.mode == "rmsprop":
+        _apply_side_infor_rmsprop()
+    elif opt.mode == "adam":
+        _apply_side_infor_adam()
+
+
+def attach_adadps(
+    cls,
+    accountant,
+    l2_norm_clip,
+    noise_multiplier,
+    lot_size,
+    batch_size,
+    dataset_size,
+    mode="rmsprop",
+    beta=0.9,
+):
+    class AdaDPSWrapper(cls):
+        """Implementation of AdaDPS proposed in
+        `Private Adaptive Optimization with Side information`
+        (https://arxiv.org/pdf/2202.05963.pdf")"""
+
+        def __init__(self, *args, **kwargs):
+            super(AdaDPSWrapper, self).__init__(*args, **kwargs)
+
+            if noise_multiplier < 0.0:
+                raise ValueError(
+                    "Invalid noise_multiplier: {}".format(noise_multiplier)
+                )
+            if l2_norm_clip < 0.0:
+                raise ValueError("Invalid l2_norm_clip: {}".format(l2_norm_clip))
+
+            self.accountant = accountant
+            self.l2_norm_clip = l2_norm_clip
+            self.noise_multiplier = noise_multiplier
+            self.batch_size = batch_size
+            self.lot_size = lot_size
+            self.beta = beta
+
+            for group in self.param_groups:
+                group["accum_grads"] = [
+                    torch.zeros_like(param.data) if param.requires_grad else None
+                    for param in group["params"]
+                ]
+
+            for group in self.param_groups:
+                group["side_information"] = [
+                    torch.zeros_like(param.data) if param.requires_grad else None
+                    for param in group["params"]
+                ]
+
+            for group in self.param_groups:
+                group["potential_momentum"] = [
+                    torch.zeros_like(param.data) if param.requires_grad else None
+                    for param in group["params"]
+                ]
+
+        def zero_grad(self):
+            super(AdaDPSWrapper, self).zero_grad()
+
+        def accumulate_grad(self):
+            _precondition_grads_with_side_info(self)
+            clip_coef = _calculate_clip_coef(self)
+            _apply_clip_coef(self, clip_coef)
+
+        def step_public(self):
+            if mode == "rmsprop":
+                _update_side_info_rmsprop(self)
+            elif mode == "adam":
+                _update_side_info_adam(self)
+
+        def step(self):
+            self.accumulate_grad()
+
+        def zero_grad_for_lot(self):
+            _clear_accumulated_grads(self)
+
+        def step_for_lot(self, *args, **kwargs):
+            _privatize_lot_grads(self)
+            super(AdaDPSWrapper, self).step(*args, **kwargs)
+            accountant.add_step_info(
+                {"sigma": self.noise_multiplier}, self.lot_size / dataset_size, 1
+            )
+
+    return AdaDPSWrapper
--- src/aijack/defense/dp/manager/dp_manager.py
+++ src/aijack/defense/dp/manager/dp_manager.py
@@ -1,7 +1,8 @@
 from torch.utils.data import DataLoader
 
 from .dataloader import LotDataLoader, PoissonSampler
-from .optimizer import attach_differential_privacy_mechanism
+from .dpoptimizer import attach_dpoptimizer
+from .adadps import attach_adadps
 
 
 class DPSGDManager:
@@ -24,7 +25,7 @@ class DPSGDManager:
         self.iterations = iterations
 
     def privatize(self, noise_multiplier):
-        dpoptimizer_class = attach_differential_privacy_mechanism(
+        dpoptimizer_class = attach_dpoptimizer(
             self.optimizer_cls,
             self.accountant,
             self.l2_norm_clip,
@@ -32,6 +33,57 @@ class DPSGDManager:
             self.lot_size,
             self.batch_size,
             len(self.dataset),
+        )
+
+        def lot_loader(dp_optimizer):
+            return LotDataLoader(
+                dp_optimizer,
+                self.dataset,
+                batch_sampler=PoissonSampler(
+                    self.dataset, self.lot_size, self.iterations
+                ),
+            )
+
+        def batch_loader(lot):
+            return DataLoader(lot, batch_size=self.batch_size, drop_last=True)
+
+        return dpoptimizer_class, lot_loader, batch_loader
+
+
+class AdaDPSManager:
+    def __init__(
+        self,
+        accountant,
+        optimizer_cls,
+        l2_norm_clip,
+        dataset,
+        lot_size,
+        batch_size,
+        iterations,
+        mode="rmsprop",
+        beta=0.9,
+    ):
+        self.accountant = accountant
+        self.optimizer_cls = optimizer_cls
+        self.l2_norm_clip = l2_norm_clip
+        self.dataset = dataset
+        self.lot_size = lot_size
+        self.batch_size = batch_size
+        self.iterations = iterations
+        self.mode = mode
+        self.beta = beta
+
+    def privatize(self, noise_multiplier):
+        dpoptimizer_class = attach_adadps(
+            self.optimizer_cls,
+            self.accountant,
+            self.l2_norm_clip,
+            noise_multiplier,
+            self.lot_size,
+            self.batch_size,
+            len(self.dataset),
+            self.mode,
+            self.beta,
         )
 
         def lot_loader(dp_optimizer):
