2ee2fdff6133bfb9051f33dae15510f0736bf123
Koukyosyumei <koukyosyumei@hotmail.com>
2023-06-06 11:49:56 +0900

    add dosctrings

--- src/aijack/defense/debugging/assertions.py
+++ src/aijack/defense/debugging/assertions.py
@@ -5,19 +5,47 @@ from .utils import Hungarian
 
 
 class MultiBoxAssertionError(AssertionError):
-    """MultiBoxAssertionError class."""
+    """Custom assertion error class for multi-box assertions"""
 
     def __init__(self, message):
+        """
+        Initialize a MultiBoxAssertionError.
+
+        Args:
+            message (str): The error message.
+        """
         super().__init__(message)
 
 
 def nearly_contains(box_1, box_2, eps):
+    """
+    Check if box_1 nearly contains box_2 with a given epsilon value.
+
+    Args:
+        box_1 (tuple|list): The coordinates of the first box in the format (x_min, y_min, x_max, y_max).
+        box_2 (tuple|list): The coordinates of the second box in the format (x_min, y_min, x_max, y_max).
+        eps (float): The epsilon value.
+
+    Returns:
+        bool: True if box_1 nearly contains box_2, False otherwise.
+    """
     return (box_1[0] + eps < box_2[2] or box_2[2] + eps < box_1[0]) and (
         box_1[1] + eps < box_2[3] or box_2[3] + eps < box_1[1]
     )
 
 
 def assert_multibox(boxes, counter_threshold=2, eps=0):
+    """
+    Assert the multi-box condition for a list of boxes.
+
+    Args:
+        boxes (list): A list of boxes. The format of each box is a tuple or list with the format of (x_min, y_min, x_max, y_max)
+        counter_threshold (int, optional): The minimum number of overlaps required for each box. Defaults to 2.
+        eps (float, optional): The epsilon value for nearly_contains function. Defaults to 0.
+
+    Raises:
+        MultiBoxAssertionError: If any box overlaps with more than counter_threshold other boxes.
+    """
     len_boxes = len(boxes)
     counter = [0 for i in range(len_boxes)]
     for i in range(len_boxes):
@@ -33,13 +61,31 @@ def assert_multibox(boxes, counter_threshold=2, eps=0):
 
 
 class FlickerException(Exception):
-    """FlickerException class."""
+    """Custom exception class for flickering detection."""
 
     def __init__(self, message):
+        """
+        Initialize a FlickerException.
+
+        Args:
+            message (str): The error message.
+        """
         super().__init__(message)
 
 
 def psnr(img_1, img_2, data_range=255, eps=1e-8):
+    """
+    Calculate the peak signal-to-noise ratio (PSNR) between two images.
+
+    Args:
+        img_1 (numpy.ndarray): The first image.
+        img_2 (numpy.ndarray): The second image.
+        data_range (int, optional): The data range of the images. Defaults to 255.
+        eps (float, optional): A small value to avoid division by zero. Defaults to 1e-8.
+
+    Returns:
+        float: The PSNR value.
+    """
     if img_1.shape != img_2.shape:
         new_shape = (
             max(img_1.shape[0], img_2.shape[0]),
@@ -60,6 +106,21 @@ def get_simillar_boxes(
     data_range=255,
     eps=1e-8,
 ):
+    """
+    Get similar boxes between two frames based on the peak signal-to-noise ratio (PSNR).
+
+    Args:
+        frame_1 (numpy.ndarray): The first frame.
+        frame_2 (numpy.ndarray): The second frame.
+        boxes_1 (list): A list of boxes in frame_1.
+        boxes_2 (list): A list of boxes in frame_2.
+        similarity_threshold (float, optional): The similarity threshold for matching boxes. Defaults to 35.
+        data_range (int, optional): The data range of the images. Defaults to 255.
+        eps (float, optional): A small value to avoid division by zero. Defaults to 1e-8.
+
+    Returns:
+        list: A list of similar boxes.
+    """
     n1 = len(boxes_1)
     n2 = len(boxes_2)
     sim_matrix = np.zeros((max(n1, n2), max(n1, n2))) + (1 / eps)
@@ -98,6 +159,20 @@ def except_flicker(
     similarity_threshold=35,
     data_range=255,
 ):
+    """
+    Check for flickering between the current frame and previous frames.
+
+    Args:
+        frames (list): A list of frames.
+        boxes_of_frames (list): A list of boxes for each frame.
+        cur_frame_id (int, optional): The index of the current frame. Defaults to -1.
+        window_size (int, optional): The number of previous frames to consider. Defaults to 10.
+        similarity_threshold (float, optional): The similarity threshold for matching boxes. Defaults to 35.
+        data_range (int, optional): The data range of the images. Defaults to 255.
+
+    Raises:
+        FlickerException: If flickering is detected between the current frame and a previous frame.
+    """
     cur_frame = frames[cur_frame_id]
     cur_boxes = boxes_of_frames[cur_frame_id]
     for i in range(
