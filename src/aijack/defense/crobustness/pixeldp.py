481f0d79e038cb9306d15df4e6281a45f368541a
Koukyosyumei <koukyosyumei@hotmail.com>
2023-08-17 18:46:00 +0900

    add pixeldp

--- src/aijack/defense/crobustness/pixeldp.py
+++ src/aijack/defense/crobustness/pixeldp.py
@@ -1,21 +1,15 @@
-import torch
 import math
-from math import sqrt, log
-from statsmodels.stats.proportion import proportion_confint
+from math import log, sqrt
+
 import numpy as np
-
-
-def clopper_pearson_interval(self, num_success, num_total, alpha):
-    return proportion_confint(num_success, num_total,
-                              alpha=2*alpha, method="beta")
-
-
-def hoeffding_interval(self, e_ax, num_total, alpha, k):
-    bound = sqrt(log(2*k/alpha) / (2*num_total))
-    return e_ax - bound, e_ax + bound
-
+import torch
+from statsmodels.stats.proportion import proportion_confint
 
 log1c25 = log(1.25)
+
+
+def clopper_pearson_interval(num_success, num_total, alpha):
+    return proportion_confint(num_success, num_total, alpha=2 * alpha, method="beta")
 
 
 def gaus_delta_term(delta):
@@ -28,10 +22,19 @@ def get_maximum_L_laplace(lower_bound, upper_bound, L, dp_eps):
     return L * log(lower_bound / upper_bound) / (2 * dp_eps)
 
 
-def get_maximum_L_gaussian(p_max_lb, p_sec_ub, attack_size,
-                           dp_epsilon, dp_delta,
-                           delta_range=list(np.arange(0.001, 0.3, 0.001)),
-                           eps_min=0.0, eps_max=1.0, tolerance=0.001):
+def get_maximum_L_gaussian(
+    p_max_lb,
+    p_sec_ub,
+    attack_size,
+    dp_epsilon,
+    dp_delta,
+    delta_range=list(np.arange(0.001, 0.3, 0.001)),
+    eps_min=0.0,
+    eps_max=1.0,
+    tolerance=0.001,
+):
+    # Based on the original implementation:
+    # https://github.com/columbia/pixeldp/blob/master/models/utils/robustness.py
     if p_max_lb <= p_sec_ub:
         return 0.0
 
@@ -39,13 +42,12 @@ def get_maximum_L_gaussian(p_max_lb, p_sec_ub, attack_size,
     for delta in delta_range:
         eps = (eps_min + eps_max) / 2
         while eps_min < eps and eps_max >= eps:
-            L_temp = attack_size *  \
-                (eps / dp_epsilon) *  \
-                (gaus_delta_term(
-                    dp_delta) / gaus_delta_term(delta))
-            if p_max_lb >= math.e ** (2 * eps) *\
-                    p_sec_ub + (1 +
-                                math.e ** eps) * delta:
+            L_temp = (
+                attack_size
+                * (eps / dp_epsilon)
+                * (gaus_delta_term(dp_delta) / gaus_delta_term(delta))
+            )
+            if p_max_lb >= math.e ** (2 * eps) * p_sec_ub + (1 + math.e**eps) * delta:
                 if L_temp > max_r:
                     max_r = L_temp
                     eps_min = eps
@@ -61,12 +63,11 @@ def get_maximum_L_gaussian(p_max_lb, p_sec_ub, attack_size,
     return max_r
 
 
-def get_certified_robustness_size_argmax(counts, eta, L, eps,
-                                         delta, mode="gaussian"):
+def get_certified_robustness_size_argmax(counts, eta, L, eps, delta, mode="gaussian"):
     total_counts = torch.sum(counts)
     sorted_counts, _ = torch.sort(counts)
-    lb = clopper_pearson_interval(sorted_counts[-1], total_counts, eta)
-    ub = clopper_pearson_interval(sorted_counts[-2], total_counts, eta)
+    lb = clopper_pearson_interval(sorted_counts[-1], total_counts, eta)[0]
+    ub = clopper_pearson_interval(sorted_counts[-2], total_counts, eta)[1]
 
     if mode == "laplace":
         return get_maximum_L_laplace(lb, ub, L, eps)
@@ -77,13 +78,24 @@ def get_certified_robustness_size_argmax(counts, eta, L, eps,
 
 
 class PixelDP(torch.nn.Module):
-    def __init__(self, model, num_classes,
-                 L, eps, delta,
-                 n_population_mc, batch_size_mc, eta,
-                 mode="laplace",
-                 sensitivity=1):
+    def __init__(
+        self,
+        model,
+        num_classes,
+        L,
+        eps,
+        delta,
+        n_population_mc=1000,
+        batch_size_mc=32,
+        eta=0.05,
+        mode="laplace",
+        sensitivity=1,
+    ):
+        super(PixelDP, self).__init__()
+
         self.model = model
         self.num_classes = num_classes
+        self.L = L
         self.eps = eps
         self.delta = delta
         self.n_population_mc = n_population_mc
@@ -93,10 +105,17 @@ class PixelDP(torch.nn.Module):
 
         if self.mode == "laplace":
             self.sigma = sensitivity * L / eps
+            self.dist = torch.distributions.laplace.Laplace(0, self.sigma)
         elif self.mode == "gaussian":
             self.sigma = gaus_delta_term(delta) * sensitivity * L / eps
         else:
             raise ValueError(f"{mode} is not supported")
+
+    def sample_noise(self, x):
+        if self.mode == "laplace":
+            return self.dist.sample(x.shape).to(x.device)
+        else:
+            return torch.randn_like(x, device=x.device) * self.sigma
 
     def forward(self, x):
         if self.training:
@@ -105,30 +124,29 @@ class PixelDP(torch.nn.Module):
             return self.forward_eval(x)
 
     def forward_train(self, x):
-        return self.model(x + torch.randn_like(x, self.device) * self.sigma)
+        return self.model(x + self.sample_noise(x))
 
     def forward_eval(self, x):
         remaining_num = self.n_population_mc
+        input_dim = len(x.shape) - 1
         with torch.no_grad():
-            preds = torch.zeros(self.num_classes)
-            counts = torch.zeros(self.num_classes)
+            preds = torch.zeros(self.num_classes, device=x.device)
+            counts = torch.zeros(self.num_classes, device=x.device)
             while remaining_num > 0:
                 batch_size = min(self.batch_size_mc, remaining_num)
                 remaining_num -= batch_size
-                inputs = x.repeat((batch_size, 1, 1, 1))
-                noise = torch.randn_like(
-                    inputs, device=self.device) * self.sigma
+                inputs = x.repeat(tuple([batch_size] + [1] * input_dim))
+                noise = self.sample_noise(inputs)
                 tmp_pred = self.model(inputs + noise)
                 tmp_pred_argmax = tmp_pred.argmax(1)
 
-                preds += tmp_pred
+                preds += tmp_pred.sum(0)
                 for i in range(len(tmp_pred_argmax)):
                     counts[tmp_pred_argmax[i]] += 1
 
         return preds, counts
 
     def certify(self, counts):
-        return get_certified_robustness_size_argmax(counts,
-                                                    self.eta, self.L,
-                                                    self.eps, self.delta,
-                                                    self.mode)
+        return get_certified_robustness_size_argmax(
+            counts, self.eta, self.L, self.eps, self.delta, self.mode
+        )
