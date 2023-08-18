ce3573abcfd82a17ec8d46f0db92e5af74332901
Koukyosyumei <koukyosyumei@hotmail.com>
2023-08-18 18:09:58 +0900

    remove unnecessary copies

--- src/aijack/collaborative/tree/xgboost/tree.h
+++ src/aijack/collaborative/tree/xgboost/tree.h
@@ -1,39 +1,34 @@
 #pragma once
-#include <vector>
-#include <iterator>
-#include <limits>
-#include <iostream>
 #include "../core/tree.h"
 #include "node.h"
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <vector>
 
-struct XGBoostTree : Tree<XGBoostNode>
-{
-    XGBoostTree() {}
-    void fit(vector<XGBoostParty> &parties, vector<float> &y, int num_classes,
-             vector<vector<float>> &gradient, vector<vector<float>> &hessian,
-             float min_child_weight, float lam,
-             float gamma, float eps, int min_leaf, int depth,
-             int active_party_id = -1, bool use_only_active_party = false, int n_job = 1)
-    {
-        vector<int> idxs(y.size());
-        iota(idxs.begin(), idxs.end(), 0);
-        for (int i = 0; i < parties.size(); i++)
-        {
-            parties[i].subsample_columns();
-        }
-
-        dtree = XGBoostNode(parties, y, num_classes, gradient, hessian, idxs,
-                            min_child_weight, lam, gamma, eps, depth,
-                            active_party_id, use_only_active_party, n_job);
+struct XGBoostTree : Tree<XGBoostNode> {
+  XGBoostTree() {}
+  void fit(vector<XGBoostParty> &parties, vector<float> &y, int num_classes,
+           const vector<vector<float>> &gradient,
+           const vector<vector<float>> &hessian, float min_child_weight,
+           float lam, float gamma, float eps, int min_leaf, int depth,
+           int active_party_id = -1, bool use_only_active_party = false,
+           int n_job = 1) {
+    vector<int> idxs(y.size());
+    iota(idxs.begin(), idxs.end(), 0);
+    for (int i = 0; i < parties.size(); i++) {
+      parties[i].subsample_columns();
     }
 
-    XGBoostNode get_root_xgboost_node()
-    {
-        return dtree;
-    }
+    dtree = XGBoostNode(parties, y, num_classes, gradient, hessian, idxs,
+                        min_child_weight, lam, gamma, eps, depth,
+                        active_party_id, use_only_active_party, n_job);
+  }
 
-    // vector<vector<float>> predict(vector<vector<float>> X)
-    //{
-    //     return dtree.predict(X);
-    // }
+  XGBoostNode get_root_xgboost_node() { return dtree; }
+
+  // vector<vector<float>> predict(vector<vector<float>> X)
+  //{
+  //     return dtree.predict(X);
+  // }
 };
